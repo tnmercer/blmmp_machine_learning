be247088531c1f6f65dc30666559fd6891399969
Peter Paquette <github@peterpaquette.com>
2022-09-15 13:08:33 -0700

    Updated prompts.py
    Fixed logic error in prompt_file_path()
    Fixed logic error in prompt_confirm()

--- prompts.py
+++ prompts.py
@@ -3,54 +3,60 @@ import questionary as qt
 
 def welcome_message():
 
-    print()
-    print('Welcome to MMP: Retirement Prediction')
-    print()
-    print("This Monthly Money Planner application helps you plan your retirement portoflio investment strategy. This application lets you choose one of three machine learning models for prediction . Results are displayed in an interactive graph that you man inspect. Enjoy!")
-    print()
+	print()
+	print('Welcome to MMP: Retirement Prediction')
+	print()
+	print("This Monthly Money Planner application helps you plan your retirement portoflio investment strategy. This application lets you choose one of three machine learning models for prediction . Results are displayed in an interactive graph that you man inspect. Enjoy!")
+	print()
 
-    return None
+	return None
 
 def prompt_multiple_choice(question_text, question_options):
-    '''This function wraps the Questionary .select() function used to prompt the user for an answer.
-    '''
+	'''This function wraps the Questionary .select() function used to prompt the user for an answer.
+	'''
 
-    result = qt.select(
-        question_text,
-        choices = question_options
-    ).ask()
-    print()
+	debug_print('---- prompt_multiple_choice()')
 
-    return result
+	result = qt.select(
+		question_text,
+		choices = question_options
+	).ask()
+	print()
+
+	return result
 
 def prompt_single_choice(question_text):
 
-    result = qt.confirm(question_text).ask()
-    print()
+	debug_print('---- prompt_single_choice()')
+	
+	result = qt.confirm(question_text).ask()
+	print()
 
-    return result
+	return result
 
 def prompt_text_input(question_text):
+
+	debug_print('---- prompt_text_input()')
 
 	result = qt.text(question_text).ask()
 	print()
 
 	return result
 
-def prompt_file_path(question_text):
+def prompt_file_path(question_text, default='file.pkl'):
 
-	result = qt.path(
-		question_text,
-		str='./predictions/portfolio_prediction_df.pkl',
-		validate='',
-	)
+	debug_print('---- prompt_file_path()')
+
+	result = qt.path(question_text, default).ask()
 	print()
 
 	return result
 
 def prompt_confirm(question_text, qmark=':'):
 
-	result = qt.confirm(question_text, qmark=qmark)
+	debug_print('---- prompt_confirm()')
+
+	result = qt.confirm(question_text, qmark=qmark).ask()
 	print()
 
 	return result
